fb482cf26896bd1fa347f0108b8b1ef97af2ec8c
bailatrix <m.bailey.passmore@gmail.com>
2020-09-15 13:48:03 -0700

    fixed errors in search

--- src/modules/prep_IO.py
+++ src/modules/prep_IO.py
@@ -21,21 +21,24 @@ locs = { 'aofa': source_dir+'archive_of_archive',
          #'host_info': source_dir+'comet_hostfile_logs',
          #'old_pickles': source_dir+'comet_pickles'
        }
-arc_data = [ locs['arc']+'/'+host_dir+'/'+stamp 
+acct_info_locs = [ locs['job_info']+'/'+stamp for stamp in listdir(locs['job_info']) ]
+curr_data = [ locs['arc']+'/'+host_dir+'/'+stamp 
             for host_dir in listdir(locs['arc'])
             for stamp in listdir(locs['arc']+'/'+host_dir)  ]
 
 aofa_data = [ locs['aofa']+'/'+host_dir+'/'+stamp 
             for host_dir in listdir(locs['aofa'])
             for stamp in listdir(locs['aofa']+'/'+host_dir)  ]
-acct_info_locs = [ locs['job_info']+'/'+stamp for stamp in listdir(locs['job_info']) ]
+arc_data = curr_data + aofa_data
 
 ### Prep Cleaning
 
 def get_time( spec=None ):
     if type(spec) is str:
-        spec = float( spec )
-    
+        try:
+            spec = float( spec )
+        except:
+            spec = get_stamp( spec )
     return clock.strftime("%Y-%m-%dT%H:%M:%S", clock.localtime( spec ))
 
 def get_stamp( spec ):
@@ -51,6 +54,22 @@ def get_stamp( spec ):
                 return spec
             else:
                 return 0
+
+def comp_window( t_0, t_n, t_x ):
+    try:
+        
+        # tester (t_x) comes before expected initial (t_0)
+        # or after expected final (t_n)
+        # NOTE: For tester.gz files, it's possible s/e window is very small and contained completely w/in file
+        if ( t_x < t_0 ) or ( t_x > t_n ):
+            return 1
+        
+        # tester within expected s/e window
+        elif t_0 <= t_x <= t_n:
+            return 0
+        
+    except:
+        return -1            
 
 def check_static( alist ):
     return alist[1:] == alist[:-1]
@@ -68,9 +87,6 @@ def check_header( line ):
             return (chunks[0][0] == '%') or ( chunks[2].find("comet") >= 0 )
         except:
             return False
-
-#def group_from_txt(  ):
-#    PAUSED
 
 def check_job( chunk ):
     return chunk.find("-") == -1
@@ -107,6 +123,9 @@ def group_from_txt( txt_file ):
         
     return group
 
+# want to split into:
+#      1) from file and
+#      2) from search list
 def collect_ids( acct_file ):
     chunks = open_txt( acct_file )    
     test_ids = []
@@ -116,6 +135,10 @@ def collect_ids( acct_file ):
         test_ids.append(cut[0])
     
     return test_ids
+
+def collect_headers( host_gzfile ):
+    lines = unzip_txt( host_gzfile )
+    return [ line for line in lines if 'comet' in line and ' ' in line ]
 
 def quick_save( obj, label=get_time() ):
     
@@ -129,100 +152,6 @@ def quick_save( obj, label=get_time() ):
         
     except:
         "There was a problem pickling the object - Save manually."
-
-####Data Munging
-
-def info_dict( rules, info ):
-    rules_list = rules.split("|")
-    
-    if len(rules_list) != len(info):
-        return {}
-    
-    else:
-        return { rules_list[i]:info[i] for i in range(len(rules_list)) }
-
-def host_to_info_dict( zip_txt ):
-    contents = unzip_txt( zip_txt )
-    host_name = contents[1].partition(" ")[2][:11]
-    out_dict = { host_name: {} }
-    host_info = {}
-    info_dict = { "Data":{},
-                    "Job":"N/A",
-                    "Schemas":{},
-                    "Specs":[]
-                }
-    
-    for line in contents:
-            
-        if line[0] == "$":
-            info_dict["Specs"].append( format_spec( line ) )
-            
-        elif line[0] == "!":
-            info_dict["Schemas"].update( format_schema( line ) )
-        
-        else:
-            
-            if (len(line) > 0) and (len(line) < 3 or check_header( line )):
-                header_dict = format_header( line )
-                
-                if header_dict:
-                    t = header_dict["Timestamp"]
-                    host_info[ t ] = {}
-                    
-                    if check_job( header_dict["Jobid"] ):
-                        info_dict["Job"] = { "Jobid": header_dict["Jobid"] } 
-                    
-            else:
-                incoming = format_data( line )
-                info_dict["Data"].update(incoming)
-                
-                host_info[t].update( info_dict )
-                
-    out_dict[host_name].update( host_info )
-    
-    return out_dict
-
-def job_to_info_dict( txt_file_list ):
-    nodes_by_date = {}
-    unsaved = []
-
-    for date in txt_file_list:
-        try:
-            # skip alt files
-            #check_stamp = int( date[-14] )
-            
-            # read in file contents
-            contents = open_txt( date )
-            
-            # formatting
-            label = date[-14:-4]
-            rules = contents[0]
-            jobs = contents[1:]
-            
-            # template to save
-            nodes_by_date[ label ] = {}
-            nodes_by_date[ label ]["multiple"] = {}
-            nodes_by_date[ label ]["rules"] = rules
-            
-            # run through lines in file
-            for job in jobs:
-                line = job.split("|")
-                node = line[-1]
-                info = info_dict( rules, line )
-                
-                # save multiple node jobs to specified loc
-                if len(node) > 12:
-                    nodes = format_nodelist( info )
-                    for node in nodes:
-                        nodes_by_date[ label ][ "multiple" ][ node ] = info
-                
-                else:
-                    nodes_by_date[ label ][ node[:11] ] = info
-        except:
-            unsaved.append(date)
-            
-    
-    return nodes_by_date, unsaved
 
 ####Formatting
 
@@ -393,7 +322,119 @@ def timely_dict( host_data, host_name ):
             info = (stat, metric, dev, int(data[i]), stamp)
             timely_data.append( info )
     
+    for i in range(len(timely_data)):
+        datum_list = [ timely_data[i] ]
+        t_i = timely_data[i][4]
+        job_info = host_data[ host_name ][ t_i ]["Job"]
+        
+        try:
+            if "Jobid" in job_info.keys():
+                jobid = (job_info["Jobid"])
+                new_tup = tuple( datum_list.append( jobid ) )
+                timely_data[i] = new_tup
+        except:
+            continue
+    
     return timely_data
+
+####Data Munging
+
+def info_dict( rules, info ):
+    rules_list = rules.split("|")
+    
+    if len(rules_list) != len(info):
+        return {}
+    
+    else:
+        return { rules_list[i]:info[i] for i in range(len(rules_list)) }
+
+def host_to_info_dict( zip_txt ):
+    contents = unzip_txt( zip_txt )
+    host_name = contents[1].partition(" ")[2][:11]
+    out_dict = { host_name: {} }
+    host_info = {}
+    info_dict = { "Data":{},
+                    "Job":"N/A",
+                    "Schemas":{},
+                    "Specs":[]
+                }
+    
+    for line in contents:
+            
+        if line[0] == "$":
+            info_dict["Specs"].append( format_spec( line ) )
+            
+        elif line[0] == "!":
+            info_dict["Schemas"].update( format_schema( line ) )
+        
+        else:
+            
+            if (len(line) > 0) and (len(line) < 3 or check_header( line )):
+                header_dict = format_header( line )
+                
+                if header_dict:
+                    t = header_dict["Timestamp"]
+                    host_info[ t ] = {}
+                    
+                    # Collecting as dictionary to support additional accounting data
+                    if check_job( header_dict["Jobid"] ):
+                        temp_jobid = header_dict["Jobid"]
+                        info_dict["Job"] = { "Jobid": temp_jobid } 
+                    
+            else:
+                incoming = format_data( line )
+                info_dict["Data"].update( incoming )
+                
+                host_info[t].update( info_dict )
+                
+    out_dict[host_name].update( host_info )
+    
+    for host_name,host_data in out_dict.items():
+        out_dict[ host_name ][ "Timely Data" ] = timely_dict( out_dict, host_name )
+    
+    return out_dict
+
+def job_to_info_dict( txt_file_list ):
+    nodes_by_date = {}
+    unsaved = []
+
+    for date in txt_file_list:
+        try:
+            # skip alt files
+            #check_stamp = int( date[-14] )
+            
+            # read in file contents
+            contents = open_txt( date )
+            
+            # formatting
+            label = date[-14:-4]
+            rules = contents[0]
+            jobs = contents[1:]
+            
+            # template to save
+            nodes_by_date[ label ] = {}
+            nodes_by_date[ label ]["multiple"] = {}
+            nodes_by_date[ label ]["rules"] = rules
+            
+            # run through lines in file
+            for job in jobs:
+                line = job.split("|")
+                node = line[-1]
+                info = info_dict( rules, line )
+                
+                # save multiple node jobs to specified loc
+                if len(node) > 12:
+                    nodes = format_nodelist( info )
+                    for node in nodes:
+                        nodes_by_date[ label ][ "multiple" ][ node ] = info
+                
+                else:
+                    nodes_by_date[ label ][ node[:11] ] = info
+        except:
+            unsaved.append(date)
+            
+    
+    return nodes_by_date, unsaved
 
 def lookup_files( searchable_list ):
     found = []
@@ -436,7 +477,59 @@ def lookup_files( searchable_list ):
     lost = [ e for e in searchable_list if e not in found ]
     
     return found,lost
-                
+
+def deep_search_acct( file_list, search_list ):
+    jobids = [ item[3] for item in search_list ]
+    collected = []
+    
+    for i in range(len(file_list)):
+        possible = open_txt( file_list[i] )
+        
+        for jobid in jobids:
+            for chunk in possible:
+                if jobid in chunk:
+                    collected.append(chunk)
+    
+    return collected
+
+def deep_search_host( search_list ):
+    out = { item:0 for item in search_list }
+    
+    for i in range(len( arc_data )):
+        for j in range(len( search_list )):
+            filename = arc_data[i]
+            target = search_list[j]
+            
+            if target[0] in filename:
+                try:
+                    if comp_window( target[1], target[2], filename[-13:-3] ):
+                        out[ target ] = filename
+                    else:
+                        headers = collect_headers( filename )
+                        out[ target ] = { "Source":[], "Headers":[] }
+                        
+                        for k in range(len( headers )):
+                            sample = headers[k].split(" ")
+                            
+                            # if jobids match
+                            if sample[1] == target[3]:
+                                
+                                if filename not in out[ target ][ "Source" ]:
+                                    out[ target ][ "Source" ].append( filename )
+                                out[ target ]["Headers"].append(sample)
+                            
+                            # if timestamps match
+                            elif comp_window( target[1], target[2], sample[0] ) == 0:
+                                
+                                if filename not in out[ target ][ "Source" ]:
+                                    out[ target ][ "Source" ].append( filename )
+                                out[ target ]["Headers"].append(sample)
+                            
+                except:   
+                    continue
+                    
+    return out
+
 # PARAMETERS:
 # 's/e' single search from start/end (manual)
 #       ie) "Start, End: 2020-01-03T20:34:47, 2020-01-05T08:15:18"
@@ -449,7 +542,7 @@ def lookup_files( searchable_list ):
 #           search( mode='l', myJobList )
 # 'f' repeated search from nodelist%start%end (from file)
 #       ie) "Text file: your_search_file.txt"  (Note: Mismatched file contents ignored)
-def search( mode=['s/e', 's', 'l','f'], from_list=False ):
+def search( mode=['s/e', 's', 'l','f'], from_list=False, ret_form=False ):
     
     if mode == 's/e':
         t_0,t_n = input("Start, End:").replace(",", "").split(" ")
@@ -477,10 +570,18 @@ def search( mode=['s/e', 's', 'l','f'], from_list=False ):
                 elif len( obj_tup[0] ) == 1:
                     out_list.append( obj_tup[0][0], obj_tup[1:] )
             except:
-                dropped.append( obj )
+                try:
+                    if ('comet' in obj[0]) and (len(obj[0]) == 11):
+                        out_list.append( obj )
+                except:
+                    continue
                     
-        files,notFound = lookup_files(out_list)
-        return { "Found": files, "Not Found": notFound, "Unable to Search": dropped }
+        files,notFound = lookup_files( out_list )
+        
+        #if ret_form:
+            #
+        #else:
+        return { "Acct Info": deep_search_acct( files, from_list ), "Host Info": deep_search_host( out_list ) }
     
     elif mode == 'f':
         search_list = group_from_text( input("Text file:") )
